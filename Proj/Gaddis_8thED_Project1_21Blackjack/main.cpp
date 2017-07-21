c52d34fb92810f01616a633f91774aef8ac507bc
ja2241856 <jalmaraz2@student.rcc.edu>
2017-07-20 18:12:34 -0700

    Signed-off-by: ja2241856 <jalmaraz2@student.rcc.edu>

--- Proj/Gaddis_8thED_Project1_21Blackjack/main.cpp
+++ Proj/Gaddis_8thED_Project1_21Blackjack/main.cpp
@@ -26,17 +26,18 @@ int main(int argc, char** argv)
 {
     int house;
     char dealer;
-    int char playerCard;
+    int  playerCard;
     char yes;
     srand (static_cast<unsigned int>(time(0)));
-    
+    int card1 = rand()%11+1;
+    int card2 = rand()%11+1;
     cout<<"Hello Welcome to Casino Carlos"<<endl;
     cout<<"Do you want to play Blackjack? If you do enter yes"<<endl;
     
     cin>>yes;
     if (yes)
-        cout <<"Thank you, Here are your card" <<endl;
-    cout <<srand<<" , "<<srand<<endl;
+        cout <<"Thank you, Here are your cards" <<endl;
+    cout <<card1<<" , "<<card2<<endl;
     
     
     return 0;
